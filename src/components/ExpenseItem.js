747c7b0dcfbb64f8dce02bdf5190dd36e36d3058
Anita18P <anita18.palle@gmail.com>
2024-10-20 19:40:36 +0530

    styling components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
+import "./ExpenseItem.css";
 function ExpenseItem(){
     return (
-        <div>
-      <div>15 August 2023</div>
-      <div>Book</div>
-      <div>$10</div>
+        <div className='expense-item'>
+      <div className='expense-item'>15 August 2023</div>
+      <div className='expense-item__description'>Book</div>
+      <div className='expense-item__price'>$10</div>
       
-      <div>Delhi</div>
+      <div className='expense-item__location'>Delhi</div>
       
     </div>
     );
